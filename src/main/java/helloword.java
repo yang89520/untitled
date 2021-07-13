a71e3dcf2bb4d8021f295eb546c28a3a6a01d039
yiran <yiran@bigdata.com>
2021-07-13 15:54:49 +0800

    Initial commit

---
+++ src/main/java/helloword.java
@@ -0,0 +1,12 @@
+/**
+ * @program: untitled
+ * @description:
+ * @author: Mr.Yang
+ * @create: 2021-07-13 15:49
+ **/
+
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
